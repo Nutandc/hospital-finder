e7ca9ebc3fc4f13c865f8e000653bfa8303ef11b
ojha007 <ojhaamir007@gmail.com>
2020-11-12 16:04:22 +0545

    fixes

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,14 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $h = \request()->get('q');
+        $searchResults = DB::table('hospitals')
+            ->select('id', 'name', 'location', 'opening_hour', 'image')
+            ->when($h, function ($query) use ($h) {
+                $query->orWhere('name', 'like', '%' . $h . '%')
+                    ->orWhere('address', 'like', '%' . $h . '%')
+                    ->orWhere('detail', 'like', '%' . $h . '%');
+            })->get();
+        $hospitals = DB::table('hospitals')
+            ->select('id', 'name', 'location', 'opening_hour', 'image')
+            ->take(4)
+            ->get();
+        return view('welcome', compact('searchResults', 'hospitals'));
     }
+
     public function contact()
     {
         return view('contact');
